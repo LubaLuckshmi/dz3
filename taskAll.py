42cdb22d9a758a520b5cbab2166b3257b7d987c4
Lubaluckshmi <lubov.loskutova@mail.ru>
2023-04-04 10:49:03 +0300

    Решили дз к 7 уроку

--- taskAll.py
+++ taskAll.py
@@ -1,62 +1,63 @@
 '''
-Задача 6.
-Дана строка (возможно, пустая), состоящая из букв A-Z:
-AAAABBBCCXYZDDDDEEEFFFAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBB
-Нужно написать функцию RLE, которая на выходе даст строку вида:
-A4B3C2XYZD4E3F3A6B28
-И сгенерирует ошибку,если на вход пришла невалидная строка.
-Пояснения:
-Если символ встречается 1 раз, он остается без изменений;
-Если символ повторяется более 1 раза,к нему добавляется 
-количество повторений
+Винни-Пух попросил Вас посмотреть, есть ли в его стихах ритм.
+ Поскольку разобраться в его кричалках не настолько просто,
+  насколько легко он их придумывает, Вам стоит написать программу.
+   Винни-Пух считает, что ритм есть, если число слогов (т.е. число
+    гласных букв) в каждой фразе стихотворения одинаковое. 
+    Фраза может состоять из одного слова, если во фразе несколько слов,
+     то они разделяются дефисами. Фразы отделяются друг от друга 
+     пробелами. Стихотворение  Винни-Пух вбивает в программу с 
+     клавиатуры. В ответе напишите “Парам пам-пам”, если с ритмом
+      все в порядке и “Пам парам”, если с ритмом все не в порядке
+*Пример:*
+
+**Ввод:** пара-ра-рам рам-пам-папам па-ра-па-да    
+    **Вывод:** Парам пам-пам  
 '''
-def rle(input_str):
-    if not input_str.isalpha():
-        return print('Ошибка ввода')
-    else:
-        result = ""
-        last_symb = input_str[0]
-        count = 1
-        for i in range(1, len(input_str)):
-            if input_str[i] == last_symb:
-                count+=1
-            else:
-                result += last_symb
-                if count > 1:
-                    result += str(count)
-                count =1
-                last_symb = input_str[i]
-        result += last_symb
-        if count > 1:
-            result += str(count)
-    return result
 
+def rhythm(str):
+    str = str.split()
+    list_1 = []
+    for word in str:
+        sum = 0
+        for i in word:
+            if i in 'аеёиоуыэюя':
+                sum += 1
+        list_1.append(sum)
+    return len(list_1) == list_1.count(list_1[0])
+    
 
-input_str = input("Введите стороку: ").upper()
-print(rle(input_str))
+input_str = input("Введите строку: ")
+if rhythm(input_str):
+    print('Парам пам-пам')
+else:
+    print('Пам парам')
 
+''' 3адача 36: Напишите функцию print_operation_table(operation, 
+num_rows=6, num_columns=6), которая принимает в качестве аргумента 
+функцию, вычисляющую элемент по номеру строки и столбца. Аргументы 
+num_rows и num_columns указывают число строк и столбцов таблицы, 
+которые должны быть распечатаны. Нумерация строк и столбцов идет с 
+единицы (подумайте, почему не с нуля). Примечание: бинарной операцией 
+называется любая операция, у которой ровно два аргумента, как, 
+например, у операции умножения.
+*Пример:*
 
+**Ввод:** `print_operation_table(lambda x, y: x * y) ` 
+**Вывод:**
+1 2 3 4 5 6
+
+2 4 6 8 10 12 
+3 6 9 12 15 18 
+4 8 12 16 20 24
+5 10 15 20 25 30
+6 12 18 24 30 36
 '''
-Задача 3. 
-Sample Input
-["eat", "tea", "tan", "ate", "nat", “bat"]
-Sample Output
-[ ["ate", "eat", "tea"], ["nat", "tan"], ["bat"] ]
-Т. е. сгруппировать слова по "общим буквам".
-'''
-def group(s):
-    d ={}
-    for word in s:
-        key = "".join(sorted(list(word)))
-        if key in d:
-            d[key].append(word)
-        else:
-            d[key] = [word]
-    result =[]
-    for v in d.values():
-        result.append(v)
-    print(result)
 
-input_str = ["eat", "tea", "tan", "ate", "nat", "bat", "qwer", "rewq", "wate", "tawe", "awet"]
-print(input_str)
-group(input_str)
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    a = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
+    for i in a:
+        print(*[f"{x:>3}" for x in i])
+
+
+print_operation_table(lambda x, y: x * y)
